92999fc2179b4c456c0f59072e302839dc5fed43
Charashenko <charashenko@gmail.com>
2022-03-27 03:03:39 +0200

    Added filtering of students by name

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -5,6 +5,9 @@ using System.Windows;
 using System.Windows.Controls;
 using app.model;
 using app.database;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data.Entity.Core.EntityClient;
 
 namespace app
 {
@@ -38,8 +41,21 @@ namespace app
 
         private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            try
+            {
+                string query = "select * from student where first_name = @student_name";   
+                
+                SqlParameter sp = new SqlParameter("student_name", FilterBox.Text);
+                
+                Students = model.students.SqlQuery(query,sp).ToListAsync().Result;
+                if (Output != null)
+                {
+                    Output.Content = Students.Count;
+                    Listing.ItemsSource = Students;
+                }
+            }
+            catch (AggregateException) { }
         }
-
         private void StudentInfoButton_Click(object sender, RoutedEventArgs e)
         {
         }
