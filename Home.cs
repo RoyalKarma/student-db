8c7c78f0c48b8030ba1fb28a5e9570d5b232bdc5
RoyalKarma <74502461+RoyalKarma@users.noreply.github.com>
2022-03-27 20:31:16 +0200

    Changed main window to datagrid

---
+++ Home.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Windows;
+using System.Windows.Controls;
+using app.database;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data.Entity.Core.EntityClient;
+using System.Collections.ObjectModel;
+using System.Windows.Navigation;
+
+namespace app
+{
+    /// <summary>
+    /// Interaction logic for home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        private UniversityModel model = new UniversityModel();
+        private UniversityDatabaseDataSet dataSet = new UniversityDatabaseDataSet();
+        public List<student> Students { get; set; }
+
+        //public int Test { get; set; }
+
+        public Home()
+        {
+            
+            InitializeComponent();
+
+            Students = model.students.ToListAsync().Result;
+            StudentGrid.DataContext = Students;
+            
+        }
+
+    
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                string query = "select * from student where first_name = @student_name";   
+                
+                SqlParameter sp = new SqlParameter("student_name", FilterBox.Text);
+                
+                Students = model.students.SqlQuery(query,sp).ToListAsync().Result;
+                /*if (Output != null)
+                {
+                    Output.Content = Students.Count;
+                    Listing.ItemsSource = Students;
+                }*/
+            }
+            catch (AggregateException) { }
+        }
+        private void StudentInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+       
+    }
+}
