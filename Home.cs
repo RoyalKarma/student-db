639edbecc8e053fd0d71e63a957edf727945b9e7
Charashenko <charashenko@gmail.com>
2022-03-28 23:46:26 +0200

    fixed filtering

--- Home.cs
+++ Home.cs
@@ -7,6 +7,7 @@ using System.Data.SqlClient;
 using app.model;
 using System.Linq;
 
+
 namespace app
 {
     /// <summary>
@@ -15,7 +16,6 @@ namespace app
     public partial class Home : Window
     {
         private UniversityModel db = new UniversityModel();
-        private UniversityDatabaseDataSet dataSet = new UniversityDatabaseDataSet();
         public List<student> Students { get; set; }
 
         public Home()
@@ -52,20 +52,19 @@ namespace app
 
         private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-                string query = "SELECT * FROM student WHERE first_name = @student_name";   
-                
-                SqlParameter sp = new SqlParameter("student_name", FilterBox.Text);
-                
-                Students = db.students.SqlQuery(query,sp).ToListAsync().Result;
-                if (Output != null)
-                {
-                    Output.Content = Students.Count;
-                    StudentGrid.ItemsSource = Students;
-                }
-            }
-            catch (AggregateException) { }
+            var query =
+               from s in db.students where s.first_name.Contains(FilterBox.Text)
+               join shasp in db.student_has_study_program on s.id_student equals shasp.student_id
+               join p in db.study_program on shasp.program_id equals p.program_id
+               select new
+               {
+                   first_name = s.first_name,
+                   last_name = s.last_name,
+                   year = s.year,
+                   program_name = p.program_name
+               };
+
+            StudentGrid.ItemsSource = query.ToList();
         }
         private void StudentInfoButton_Click(object sender, RoutedEventArgs e)
         {
