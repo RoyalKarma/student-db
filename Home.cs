27496e4ab9d4a6d03793a8cf384e3bc298068dfa
RoyalKarma <74502461+RoyalKarma@users.noreply.github.com>
2022-04-17 12:35:22 +0200

    New project structure

---
+++ Home.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using app.database;
+using System.Data.SqlClient;
+
+using System.Linq;
+
+
+namespace app
+{
+    /// <summary>
+    /// Interaction logic for home.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+       public HomeWindow()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
---
+++ HomePage.xaml.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace app
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        UniDBEntities1 db = new UniDBEntities1();
+        CollectionViewSource studentViewSource;
+
+        public List<student> Students { get; set; }
+
+        public HomePage()
+        {
+
+            InitializeComponent();
+            studentViewSource = ((CollectionViewSource)(FindResource("studentViewSource")));
+            var query =
+                from s in db.students
+                join shasp in db.student_has_faculty on s.student_id equals shasp.student_id
+                join p in db.faculties on shasp.faculty_id equals p.faculty_id
+                select new
+                {
+                    student_id = s.student_id,
+                    first_name = s.first_name,
+                    last_name = s.last_name,
+                    year = s.year,
+                    abbrevation = p.abbrevation
+                   
+                };
+            DataContext = this;
+            db.students.Load();
+            studentViewSource.Source = query.ToList();
+
+
+
+
+        }
+
+
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var student = new student
+            {
+                first_name = "jmeno",
+                last_name = "prijmeni",
+                year = 2
+            };
+            
+                db.students.Add(student);
+                db.SaveChanges();
+
+            
+                
+        
+        }
+     
+
+
+        private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var query =
+                from s in db.students
+                join shasp in db.student_has_faculty on s.student_id equals shasp.student_id
+                join p in db.faculties on shasp.faculty_id equals p.faculty_id          
+                where s.first_name.Contains(FilterBox.Text) | p.faculty_name.Contains(FilterBox.Text)//| s.last_name.Contains(FilterBox.Text) 
+                select new
+                {
+                    student_id = s.student_id,
+                    first_name = s.first_name,
+                    last_name = s.last_name,
+                    year = s.year,
+                    abbrevation = p.abbrevation
+
+                };
+
+
+
+
+
+            studentDataGrid.ItemsSource = query.ToList();
+        }
+        private void StudentInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(studentDataGrid.SelectedValue != null)
+            {
+                //NavigationService.Navigate(new StudentInfoPage(parseID()));
+                studentDataGrid.SelectedValue = null;
+            }
+            
+            
+        }
+        
+        private int parseID() { //needed to extract year from db for filtering
+            string s = studentDataGrid.SelectedValue.ToString();
+            s = s.Substring(1, s.Length - 2).Trim().Split(',')[0].Split('=')[1].Trim();
+            return int.Parse(s); 
+        }
+    }
+}
